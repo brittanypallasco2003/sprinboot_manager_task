1025323bb4918cfbf6bdcf927a98cc0ae6a1ad73
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-18 16:52:04 -0500

    feat: agregar archivos iniciales del proyecto Spring Boot, incluyendo configuración de Maven, propiedades de aplicación

---
+++ src/main/java/com/brittany/sprinboot_manager_task/SprinbootManagerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.brittany.sprinboot_manager_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootManagerTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootManagerTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brittany/sprinboot_manager_task/SprinbootManagerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brittany.sprinboot_manager_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootManagerTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
