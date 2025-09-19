68539e0630d26b9aad73ad999f0d8eb53547cf3c
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 16:37:21 -0500

    feat(auth): agregar endpoints de registro, login y generación de JWT

---
+++ src/main/java/com/brittany/sprinboot_manager_task/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package com.brittany.sprinboot_manager_task.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brittany.sprinboot_manager_task.DTOs.Request.LoginDTO;
+import com.brittany.sprinboot_manager_task.DTOs.Request.RegisterRequestDTO;
+import com.brittany.sprinboot_manager_task.services.AuthServiceI;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthServiceI authServiceI;
+
+    public AuthController(AuthServiceI authServiceI) {
+        this.authServiceI = authServiceI;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterRequestDTO dto) {
+        return new ResponseEntity<>(authServiceI.registerUser(dto),HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody @Valid LoginDTO dto) {
+        return new ResponseEntity<>(authServiceI.loginUser(dto),HttpStatus.OK);
+    }
+    
+}
