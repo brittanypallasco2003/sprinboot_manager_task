92f3479b95ce141c44899477dca4387371c67141
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 16:38:16 -0500

    reafactor(tareas): modificar manejador de excepciones para errores de lectura de mensajes HTTP

--- src/main/java/com/brittany/sprinboot_manager_task/exceptions/TareaExceptionHandler.java
+++ src/main/java/com/brittany/sprinboot_manager_task/exceptions/TareaExceptionHandler.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.brittany.sprinboot_manager_task.DTOs.Response.ErrorResponseDTO;
 import com.brittany.sprinboot_manager_task.models.EstadoTareaEnum;
@@ -14,7 +14,7 @@ import com.brittany.sprinboot_manager_task.utils.ErrorResponseFactory;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class TareaExceptionHandler {
 
     private final ErrorResponseFactory errorResponseFactory;
@@ -32,14 +32,11 @@ public class TareaExceptionHandler {
 
         if (ex.getMessage() != null && ex.getMessage().contains("from String")) {
             message = buildEnumErrorMessage(ex.getMessage(), EstadoTareaEnum.class);
-        } else {
-            // Error de JSON mal formado
-            message = "Error al leer el cuerpo de la solicitud. Verifique el formato del JSON.";
+            ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), message, request.getRequestURI(), null);
+            return new ResponseEntity<>(response, errorStatus);
         }
+        throw ex;
 
-        ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), message, request.getRequestURI(), null);
-
-        return new ResponseEntity<>(response, errorStatus);
     }
 
     private String buildEnumErrorMessage(String originalMessage, Class<? extends Enum<?>> enumClass) {
