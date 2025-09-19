01f0f08a4005d692840e17cfe723b05e3d26514c
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-18 21:41:36 -0500

    fix: mover modelos, servicios y repos dentro del paquete principal para que Spring Boot los detecte

--- src/main/java/com/brittany/models/EstadoTareaEnum.java
+++
@@ -1,7 +0,0 @@
-package com.brittany.models;
-
-public enum EstadoTareaEnum {
-PENDIENTE,
-EN_PROGRESO,
-COMPLETADA
-}
--- src/main/java/com/brittany/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brittany.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.brittany.models.UsuarioModel;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-
-}
