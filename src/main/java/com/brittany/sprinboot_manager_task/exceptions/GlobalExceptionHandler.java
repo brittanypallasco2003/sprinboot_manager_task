c561a633b6fffbae2d38f394c5bef82682ad4882
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 16:32:53 -0500

    feat(auth): añadir excepción para el manejo de credenciales incorrectas

--- src/main/java/com/brittany/sprinboot_manager_task/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/brittany/sprinboot_manager_task/exceptions/GlobalExceptionHandler.java
@@ -2,18 +2,21 @@ package com.brittany.sprinboot_manager_task.exceptions;
 
 import java.util.stream.Collectors;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.brittany.sprinboot_manager_task.DTOs.Response.ErrorResponseDTO;
 import com.brittany.sprinboot_manager_task.utils.ErrorResponseFactory;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final ErrorResponseFactory errorResponseFactory;
@@ -23,6 +26,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
+    @Order(20)
     public ResponseEntity<?> handleAllExceptions(Exception ex, HttpServletRequest request) {
         HttpStatus errorStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -32,21 +36,45 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, errorStatus);
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAuthenticationException(AuthenticationException ex,
+                                                                           HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorResponseDTO response = errorResponseFactory.build(
+                status.value(),
+                status.name(),
+                "Credenciales inválidas o no proporcionadas",
+                request.getRequestURI(),
+                null
+        );
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+            HttpServletRequest request) {
+
+        HttpStatus errorStatus = HttpStatus.BAD_REQUEST;
+        String message = "Error al leer el cuerpo de la solicitud. Verifique el formato del JSON.";
+
+        ErrorResponseDTO response = errorResponseFactory.build(
+                errorStatus.value(),
+                errorStatus.name(),
+                message,
+                request.getRequestURI(),
+                null
+        );
+
+        return new ResponseEntity<>(response, errorStatus);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex,
             HttpServletRequest request) {
         HttpStatus errorStatus = HttpStatus.BAD_REQUEST;
-        // ErrorResponseDTO response = mapErrorResponseDTO(errorStatus.value(),
-        // errorStatus.name(), ex.getMessage(),
-        // request.getRequestURI(),
-        // ex.getBindingResult()
-        // .getFieldErrors()
-        // .stream()
-        // .map(fieldError -> fieldError.getField() + ": " +
-        // fieldError.getDefaultMessage())
-        // .collect(Collectors.toList()));
 
-        ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), ex.getMessage(),
+        ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), "Error de Validación en los campos",
                 request.getRequestURI(),
                 ex.getBindingResult().getFieldErrors().stream()
                         .map((fieldE) -> fieldE.getField() + " : " + fieldE.getDefaultMessage())
