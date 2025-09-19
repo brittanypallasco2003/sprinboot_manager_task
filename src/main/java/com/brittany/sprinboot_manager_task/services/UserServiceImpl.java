180743d9e17f4a1f20eec75fb7e20089c1522273
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 16:30:03 -0500

    fix(usuarios): agregar anotación @Service en UserServiceImpl

--- src/main/java/com/brittany/sprinboot_manager_task/services/UserServiceImpl.java
+++ src/main/java/com/brittany/sprinboot_manager_task/services/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.brittany.sprinboot_manager_task.services;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.brittany.sprinboot_manager_task.repositories.UsuarioRepository;
 
+@Service
 public class UserServiceImpl implements UserServiceI{
 
     private final UsuarioRepository usuarioRepository;
