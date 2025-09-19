881c5e6ecc2b432ee9851582b2d57065edb90e9d
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 10:39:06 -0500

    feat(usuarios): agregar método existsByEmail en UsuarioRepository para verificar existencia de usuario por email

--- src/main/java/com/brittany/sprinboot_manager_task/repositories/UsuarioRepository.java
+++ src/main/java/com/brittany/sprinboot_manager_task/repositories/UsuarioRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.brittany.sprinboot_manager_task.models.UsuarioModel;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+   boolean existsByEmail(String email);
 
 }
