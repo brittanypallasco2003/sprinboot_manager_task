8ad49c3f065fdb39917824d295c8667ade31995a
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 16:28:51 -0500

    feat(auth): agregar método findByEmail en UsuarioRepository para búsqueda de usuarios por correo electrónico

--- src/main/java/com/brittany/sprinboot_manager_task/repositories/UsuarioRepository.java
+++ src/main/java/com/brittany/sprinboot_manager_task/repositories/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.brittany.sprinboot_manager_task.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brittany.sprinboot_manager_task.models.UsuarioModel;
 
+
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
    boolean existsByEmail(String email);
+   Optional<UsuarioModel> findByEmail(String email);
 
 }
