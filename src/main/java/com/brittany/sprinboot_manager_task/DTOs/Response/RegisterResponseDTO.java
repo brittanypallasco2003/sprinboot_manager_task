34bc4e8553481afc1831bad1a72b5604314c555c
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 22:07:28 -0500

    fix(tareas): ajustar nombre de propiedad en DTO para serialización con Jackson

--- src/main/java/com/brittany/sprinboot_manager_task/DTOs/Response/RegisterResponseDTO.java
+++ src/main/java/com/brittany/sprinboot_manager_task/DTOs/Response/RegisterResponseDTO.java
@@ -3,6 +3,6 @@ package com.brittany.sprinboot_manager_task.DTOs.Response;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({ "mensaje", "id", "nombre", "email" })
-public record RegisterResponseDTO(String mensaje, Long userId, String nombre, String email) {
+public record RegisterResponseDTO(String mensaje, Long id, String nombre, String email) {
 
 }
