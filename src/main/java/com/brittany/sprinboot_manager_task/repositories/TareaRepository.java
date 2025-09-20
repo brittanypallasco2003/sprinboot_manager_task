d4969c30567dc8ab007e74f12f0a03bb5f95e555
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 19:53:49 -0500

    feat(tareas): definir método de repositorio para listar todas las tareas de un usuario

--- src/main/java/com/brittany/sprinboot_manager_task/repositories/TareaRepository.java
+++ src/main/java/com/brittany/sprinboot_manager_task/repositories/TareaRepository.java
@@ -1,10 +1,13 @@
 package com.brittany.sprinboot_manager_task.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brittany.sprinboot_manager_task.models.TareaModel;
+import com.brittany.sprinboot_manager_task.models.UsuarioModel;
 
 public interface TareaRepository extends JpaRepository<TareaModel, Long>{
-    
+    List<TareaModel> findByUsuario(UsuarioModel usuario);
 
 }
