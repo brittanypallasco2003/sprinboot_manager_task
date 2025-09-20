a3a72fbb2586d528d044d385e3700c9883a2369b
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 22:04:17 -0500

    fix(tareas): reemplazar Optional como retorno por DTO de respuesta

--- src/main/java/com/brittany/sprinboot_manager_task/services/TareaServiceI.java
+++ src/main/java/com/brittany/sprinboot_manager_task/services/TareaServiceI.java
@@ -1,7 +1,6 @@
 package com.brittany.sprinboot_manager_task.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.brittany.sprinboot_manager_task.DTOs.Request.TareaRequestDTO;
 import com.brittany.sprinboot_manager_task.DTOs.Response.TareaResponseDTO;
@@ -10,11 +9,11 @@ public interface TareaServiceI {
 
     List<TareaResponseDTO> getAllTasksByUser();
 
-    Optional<TareaResponseDTO> getTaskById(Long id);
+    TareaResponseDTO getTaskById(Long id);
 
     TareaResponseDTO createTask(TareaRequestDTO dto);
 
-    Optional<TareaResponseDTO> updateTask(TareaRequestDTO dto);
+    TareaResponseDTO updateTask(Long id, TareaRequestDTO dto);
 
     void deleteTask(Long id);
 
