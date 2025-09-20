f31871cd0e566508469a189326f519f82e842607
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 19:49:13 -0500

    refactor(tareas): añadir el campo id de la tarea en el dto de la respuesta

--- src/main/java/com/brittany/sprinboot_manager_task/DTOs/Response/TareaResponseDTO.java
+++ src/main/java/com/brittany/sprinboot_manager_task/DTOs/Response/TareaResponseDTO.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import com.brittany.sprinboot_manager_task.models.EstadoTareaEnum;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({ "userId", "titulo", "description", "estado", "createdAt", "updatedAt" })
-public record TareaResponseDTO(Long userId, String titulo, String description, EstadoTareaEnum estado,
-        LocalDateTime createdAt, LocalDateTime updatedAt) {
+@JsonPropertyOrder({ "id", "titulo", "description", "estado", "createdAt", "updatedAt", "userId" })
+public record TareaResponseDTO(Long id, String titulo, String description, EstadoTareaEnum estado,
+        LocalDateTime createdAt, LocalDateTime updatedAt, Long userId) {
 }
