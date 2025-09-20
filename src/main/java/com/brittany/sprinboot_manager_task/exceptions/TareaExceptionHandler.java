62880ee71d914b3bad22dc68ef5e70f7513684d4
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 22:11:56 -0500

    feat(auth): agregar handler para excepciones de acceso no autorizado

--- src/main/java/com/brittany/sprinboot_manager_task/exceptions/TareaExceptionHandler.java
+++ src/main/java/com/brittany/sprinboot_manager_task/exceptions/TareaExceptionHandler.java
@@ -2,9 +2,11 @@ package com.brittany.sprinboot_manager_task.exceptions;
 
 import java.util.Arrays;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +17,7 @@ import com.brittany.sprinboot_manager_task.utils.ErrorResponseFactory;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
+@Order(1)
 public class TareaExceptionHandler {
 
     private final ErrorResponseFactory errorResponseFactory;
@@ -32,11 +35,22 @@ public class TareaExceptionHandler {
 
         if (ex.getMessage() != null && ex.getMessage().contains("from String")) {
             message = buildEnumErrorMessage(ex.getMessage(), EstadoTareaEnum.class);
-            ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), message, request.getRequestURI(), null);
+            ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), message,
+                    request.getRequestURI(), null);
             return new ResponseEntity<>(response, errorStatus);
         }
         throw ex;
 
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex,
+            HttpServletRequest request) {
+
+        HttpStatus errorStatus = HttpStatus.FORBIDDEN;
+        ErrorResponseDTO response = errorResponseFactory.build(errorStatus.value(), errorStatus.name(), ex.getMessage(),
+                request.getRequestURI(), null);
+        return new ResponseEntity<>(response, errorStatus);
     }
 
     private String buildEnumErrorMessage(String originalMessage, Class<? extends Enum<?>> enumClass) {
