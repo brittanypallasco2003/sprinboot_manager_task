f7673404e226339dd458693360f39fa794e2fe93
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-19 19:52:29 -0500

    fix(auth): agregar anotación @Service en AuthenticatedUserServiceImpl

--- src/main/java/com/brittany/sprinboot_manager_task/services/AuthenticatedUserServiceImpl.java
+++ src/main/java/com/brittany/sprinboot_manager_task/services/AuthenticatedUserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.brittany.sprinboot_manager_task.services;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import com.brittany.sprinboot_manager_task.models.UsuarioModel;
 import com.brittany.sprinboot_manager_task.repositories.UsuarioRepository;
 
+@Service
 public class AuthenticatedUserServiceImpl implements AuthenticatedUserServiceI {
 
     private final UsuarioRepository userRepository;
